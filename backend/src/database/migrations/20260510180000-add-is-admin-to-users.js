e81042dfeb7aa86eec3c99587080bd5877fcd52f
renatocosta0 <Renato.costa626@gmail.com>
2026-05-11 22:19:10 +0100

    fix: make is_admin migration idempotent

--- backend/src/database/migrations/20260510180000-add-is-admin-to-users.js
+++ backend/src/database/migrations/20260510180000-add-is-admin-to-users.js
@@ -2,14 +2,20 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn("users", "is_admin", {
-      type: Sequelize.BOOLEAN,
-      allowNull: false,
-      defaultValue: false,
-    });
+    const table = await queryInterface.describeTable('users');
+    if (!table.is_admin) {
+      await queryInterface.addColumn('users', 'is_admin', {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      });
+    }
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn("users", "is_admin");
+    const table = await queryInterface.describeTable('users');
+    if (table.is_admin) {
+      await queryInterface.removeColumn('users', 'is_admin');
+    }
   },
 };
